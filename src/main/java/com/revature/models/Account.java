2932f3696b83e3a536da2ef5163992dadfa064ea
smooses <sandra.moose@gmail.com>
2020-07-09 22:09:51 -0700

    Basic Models Added

---
+++ src/main/java/com/revature/models/Account.java
@@ -0,0 +1,70 @@
+package com.revature.models;
+
+public class Account {
+	  private int accountId; // primary key
+	  private double balance;  // not null
+	  private AccountStatus status;
+	  private AccountType type;
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Account(int accountId, double balance, AccountStatus status, AccountType type) {
+		super();
+		this.accountId = accountId;
+		this.balance = balance;
+		this.status = status;
+		this.type = type;
+	}
+	public int getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public AccountStatus getStatus() {
+		return status;
+	}
+	public void setStatus(AccountStatus status) {
+		this.status = status;
+	}
+	public AccountType getType() {
+		return type;
+	}
+	public void setType(AccountType type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "Account [accountId=" + accountId + ", balance=" + balance + ", status=" + status + ", type=" + type
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + accountId;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (accountId != other.accountId)
+			return false;
+		return true;
+	}
+
+
+}
---
+++ src/main/java/com/revature/models/User.java
@@ -0,0 +1,97 @@
+package com.revature.models;
+
+public class User {
+	  private int userId; // primary key
+	  private String username; // not null, unique
+	  private String password; // not null
+	  private String firstName; // not null
+	  private String lastName; // not null
+	  private String email; // not null
+	  private Role role;
+	
+	  
+	public User() {
+		super();
+	}
+	
+	public User(int userId, String username, String password, String firstName, String lastName, String email,
+			Role role) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.role = role;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", email=" + email + ", role=" + role + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + userId;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}  
+}
